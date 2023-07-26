8288c8bde7175c3412fcaccfe2089c1f7751d30d
gjjlne <140214318+gjjlne@users.noreply.github.com>
2023-07-26 18:34:56 -0300

    atualização

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const score = document.querySelector('.score')
 const nailson = document.querySelector('.nailson')
 const livro = document.querySelector('.livro')
+const nuvem = document.querySelector('.nuvem')
 let aljump = false;
 let count = 0
 
@@ -19,9 +20,10 @@ const jump = () => {
 const loop = setInterval(() => {
 
     const posicaolivro = livro.offsetLeft;
+    const posicaonuvem = nuvem.offsetLeft;
     const posicaonailson = +window.getComputedStyle(nailson).bottom.replace('px', '');
 
-    if (posicaolivro <= 120 && posicaolivro > 0 && posicaonailson < 80 && !aljump) {
+    if (posicaolivro <= 105 && posicaolivro > 0 && posicaonailson < 80 && !aljump) {
         //alert(`Game over! Seu score foi: ${count}`);//
 
        livro.style.animation = 'none'
@@ -29,6 +31,9 @@ const loop = setInterval(() => {
  
        nailson.style.animation = 'none'
        nailson.style.bottom = `${posicaonailson}px`;
+
+       nuvem.style.animation = 'none'
+       nuvem.style.left = `${posicaolivro}px`;
 
        clearInterval(loop);
     }
